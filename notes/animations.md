f1d23330040367079b3897feea33ae228a58fa8e
Victor J. Bezak <victorjbezak@gmail.com>
2021-07-29 09:10:59 -0500

    working on animation for star rotation

---
+++ notes/animations.md
@@ -0,0 +1,8 @@
+# Animations
+
+There are three elements involved in creating animations: keyframes, **KeyframeTrack**, and **Animation Clip**
+
+
+KeyframeTrack -> AnimationClip -> AnimationMixer -> AnimationAction
+
+track -> clip -> mixer -> action
--- src/js/index.js
+++ src/js/index.js
@@ -3,23 +3,36 @@ import mesh from './mesh.js'
 import listener from './events.js'
 import * as dat from 'dat.gui'
 
+let dayChanging = false;
+let starsRotation = 0
+const starPosition = {
+  x: 2,
+  y: 2,
+  z: 0
+}
+
 // Cameras
 const camera = new THREE.PerspectiveCamera(60, window.innerWidth / window.innerHeight)
 camera.position.set(1, 1.5, 6)
 
 // Lights
 const ambientLight = new THREE.AmbientLight('#f5f3ce', 0.3)
+
 const moonLight = new THREE.DirectionalLight('#f5f3ce', 1)
 moonLight.castShadow = true
 moonLight.intensity = 0.25;
+moonLight.position.set(-starPosition.x, -starPosition.y, -starPosition.z)
+mesh.moon.position.set(-starPosition.x, -starPosition.y, -starPosition.z)
 
 const sunLight = new THREE.DirectionalLight('#fdfbd3', 1)
 sunLight.castShadow = true
 sunLight.intensity = 0.8;
+sunLight.position.set(starPosition.x, starPosition.y, starPosition.z)
+mesh.sun.position.set(starPosition.x, starPosition.y, starPosition.z)
 
 // Groups
-const stars = new THREE.Group()
-stars.add(mesh.moon, mesh.sun, moonLight, sunLight)
+const starsGroup = new THREE.Group()
+starsGroup.add(mesh.moon, mesh.sun, moonLight, sunLight)
 
 // Scene
 const scene = new THREE.Scene()
@@ -28,7 +41,7 @@ scene.add(
   ambientLight,
   mesh.ground,
   mesh.house,
-  stars
+  starsGroup
 )
 
 // Renderer
@@ -40,40 +53,65 @@ renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
 renderer.shadowMap.enabled = true
 
 // Animation
-const clock = new THREE.Clock()
+const keyframeTrack = new THREE.NumberKeyframeTrack(
+  ".rotateZ",
+  [0, 3, 6, 9],
+  [ 0,
+    Math.PI/4,
+    Math.PI/2,
+    Math.PI
+  ]
+)
+// const keyframeTrack = new THREE.NumberKeyframeTrack(
+//   ".rotation",
+//   [0, 1, 2, 3],
+//   [0, Math.PI/4, Math.PI/2, Math.PI]
+// )
 
-function rotateStars(elapsed) {
-  // const x = Math.sin(elapsed) * 3
-  // const y = Math.cos(elapsed) * 3
-  // const z = Math.cos(elapsed)
-  const x = isDay ? 2 : -2
-  const y = isDay ? 2 : -2
-  const z = 0
-  
-  sunLight.position.set(x, y, z)
-  mesh.sun.position.set(x, y, z)
-  
-  moonLight.position.set(-x, -y, -z)
-  mesh.moon.position.set(-x, -y, -z)
+const starsClip = new THREE.AnimationClip("starsRotate", -1, [
+  keyframeTrack
+])
+// const starsMixer = new THREE.AnimationMixer(starsGroup)
+const starsMixer = new THREE.AnimationMixer(mesh.house)
+const starsAction = starsMixer.clipAction(starsClip)
+starsAction.play()
+console.log(keyframeTrack)
+
+// console.log(starsClip)
+
+function rotateStars() {
+  console.log(starsAction)
+  starsAction.play();
 }
 
+const clock = new THREE.Clock()
+
 function tick() {
-  const elapsedTime = clock.getElapsedTime()
-  rotateStars(elapsedTime)
+  const elapsedTime = clock.getDelta()
+  starsMixer.update(elapsedTime)
+  // rotateStars()
   
-  camera.position.x = Math.cos(elapsedTime / 2) * 4
-  camera.position.z = Math.sin(elapsedTime / 2) * 4
+  // camera.position.x = Math.cos(elapsedTime / 2) * 4
+  // camera.position.z = Math.sin(elapsedTime / 2) * 4
   camera.lookAt(mesh.house.position)
+
+  // if (dayChanging) {
+  //   if (starsRotation < Math.PI) {
+  //     rotateStars()
+  //   } else {
+  //     starsRotation = 0
+  //     dayChanging = false
+  //   }
+  // }
 
   renderer.render(scene, camera)
 
   window.requestAnimationFrame(tick)
 }
 
-let isDay = true;
 
 document.getElementById('day-night-btn')
-  .addEventListener('click', () => isDay = !isDay)
+  .addEventListener('click', rotateStars)
 
 tick()
 
